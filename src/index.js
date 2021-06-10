9467e6e150f7940732bd0f2b25946c847b20a818
Nir Shahaf <nirshahaf@Nirs-MacBook-Pro.local>
2021-06-10 20:42:31 +0300

    adding default rout for aws health

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const app = require('./app')
 
 const port = process.env.PORT
 
+app.use('/', (req, res) => {
+    res.send(ok)
+})
+
 app.listen(port, () => {
     console.log(`Sever is up on port ${port}`)
 })
